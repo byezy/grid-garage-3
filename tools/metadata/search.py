d332189357f7a5a0a4b475af620f67f3a442ca6d
david.bye <david.bye@environment.nsw.gov.au>
2017-05-15 09:55:27 +1000

    test commit in branch

--- tools/metadata/search.py
+++ tools/metadata/search.py
@@ -1,15 +1,15 @@
 from base.base_tool import BaseTool
-from base.class_decorators import geodata, results
+import base.results
 from base.method_decorators import input_tableview, input_output_table
-
+from base.method_decorators import input_tableview, input_output_table_with_output_affixes, parameter, transform_methods, raster_formats
 tool_settings = {"label": "Search",
                  "description": "Search for metadata",
                  "can_run_background": "True",
                  "category": "Metadata TODO"}
 
 
-@geodata
-@results
+# @geodata
+@base.results.result
 class SearchMetadataTool(BaseTool):
     def __init__(self):
         BaseTool.__init__(self, tool_settings)
@@ -28,5 +28,64 @@ class SearchMetadataTool(BaseTool):
         self.log.info(data)
         # TODO
         return
+
+
+
+    # geodata = run['item']
+    # base = tool.splitext(geodata)[0]
+    #
+    # tool.info("Analysing {0}".format(geodata))
+    #
+    # desc = tool.describe(geodata)
+    # arc_retrieved = desc.metadataRetrieved
+    #
+    #
+    # xml_file = base + ".xml"
+    # xml = None
+    # if tool.geodata_exists(xml_file):
+    #     with open(xml_file, "r") as xmlfile:
+    #         xml = "".join(line.rstrip() for line in xmlfile)
+    # else:
+    #     xml_file = "{0} does not exist".format(xml_file)
+    #
+    # tip_file = base + ".tip"
+    # tip = None
+    # if tool.geodata_exists(tip_file):
+    #     lines = [line.rstrip() for line in open(tip_file)]
+    #     lines = [l.replace(":", "=", 1) for l in lines]
+    #     # lines = ["title={0}".format(l) for l in lines if not ":"]
+    #     lined = {}
+    #     for line in lines:
+    #         if ":" not in line:
+    #             line = "title={0}".format(line)
+    #         p = line.split("=")
+    #         if len(p) == 1:
+    #             p.insert(0, "title=")
+    #         elif len(p) > 2:
+    #             s = ""
+    #             for i in range(1, len(p)):
+    #                 s += p[i]
+    #             p[1] = s
+    #         lined[p[0]] = p[1]
+    #     tip = str(lined)
+    #     # with open(tip_file, "r") as tipfile:
+    #     #     tip = "||".join(line.rstrip() for line in tipfile)
+    #         # tiplines = tip.split("||")
+    #         # title = tiplines[0]
+    #         # tool.info(title)
+    #         # tool.info(tiplines[1:])
+    #         # tool.info([t.split(":") for t in tiplines[1:]])
+    #         # try:
+    #         #     tips = {k: v for k, v in [t.split(":") for t in tiplines[1:]]}
+    #         # except:
+    #         #     tips = "Error parsing tip file"
+    #
+    # else:
+    #     tip_file = "{0} does not exist".format(tip_file)
+    #
+    #
+    # return {"item": geodata, "metadata": "to do",
+    #         "arc_retrieved": arc_retrieved, "xml_file": xml_file,
+    #          "xml": xml, "tip_file": tip_file, "tip": tip}
 
 
--- tools/metadata/tips_from_template.py
+++ tools/metadata/tips_from_template.py
@@ -30,3 +30,72 @@ class TipsFromTemplateMetadataTool(BaseTool):
         return
 
 
+def parse(run):
+    """
+    Clip raster based on current run parameters.
+
+    #Clip_management (in_raster, rectangle, out_raster, {in_template_dataset},
+    #                                      {nodata_value}, {clipping_geometry})
+    ======== ===================================================================
+    Arg1     run - A dictionary of run-time objects
+    Return   None
+    Raises   None
+    Effect   A new dataset that is a clip of the original
+    ======== ===================================================================
+    """
+    # tool.info(run['item_row'])
+    tip_file = run['item_row'][1]
+
+    assert tool.geodata_exists(tip_file), "Tip file {0} does not " \
+                                          "exist".format(tip_file)
+
+    tool.info("Parsing {0}".format(tip_file))
+
+    tips = ODict()
+    tips["header"] = ""
+
+    with open(tip_file, "r") as tipfile:
+        buf = "header"
+        for line in tipfile:
+            s = line.strip().encode("ascii").split(":", 1)
+            if len(s) > 1:
+                s1, s2 = s
+                buf = s1
+            else:
+                s1 = s[0]
+                s2 = None
+            if not s2:
+                tips[buf] += s1
+            else:
+                tips[s1] = s2
+
+    tips["field_order"] = tips.keys()
+
+    res = {"item": run["item"], "metadata": "to do", "tip_file": tip_file}
+    res.update(tips)
+
+    return res
+
+
+# ------------------------------------------------------------------------------
+# execute
+
+
+# def main():
+#     """Wraps executable code away from naughty loading.
+#
+#     For standard Grid-Garage tools we build the GridGarageTool class instance
+#     with our custom parameters and execute the required methods.
+#     ======== ===================================================================
+#     Args     Argv entries as described by the global dictionary 'par'
+#     Return   A table listing geodata found
+#     Raises   Nothing explicitly
+#     Effect   Geodata listed and catalogued
+#     ======== ===================================================================
+#     """
+#     # place tool ref in global namespace and construct the tool class instance
+#     global tool
+#     tool = GridGarageTool(__file__, __version__, par)
+#
+#     # iterate a function over the items, saving the return
+#     tool.run_f_iterate(parse, add_to_results=True)
