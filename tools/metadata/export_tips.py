f7e9886b20b6599aed53a1edeca054cdd218eeba
david.bye <david.bye@environment.nsw.gov.au>
2017-05-15 14:31:04 +1000

    metatools work

--- tools/metadata/export_tips.py
+++ tools/metadata/export_tips.py
@@ -1,31 +1,60 @@
 from base.base_tool import BaseTool
-from base.class_decorators import geodata, results
+import base.results
 from base.method_decorators import input_tableview, input_output_table
 
 tool_settings = {"label": "Export tips",
                  "description": "Exports tips...",
                  "can_run_background": "True",
-                 "category": "Metadata TODO"}
+                 "category": "Metadata"}
 
 
-@geodata
-@results
+@base.results.result
 class ExportTipsMetadataTool(BaseTool):
     def __init__(self):
         BaseTool.__init__(self, tool_settings)
-        self.execution_list = [self.start_iteration]
+
+        self.execution_list = [self.iterate]
 
     @input_tableview("geodata_table", "Table of Geodata", False, ["geodata:geodata:"])
     @input_output_table
     def getParameterInfo(self):
+
         return BaseTool.getParameterInfo(self)
 
-    def start_iteration(self):
-        self.iterate_function_on_parameter(self.process, "geodata_table", ["geodata"])
+    def iterate(self):
+
+        self.iterate_function_on_parameter(self.export, "geodata_table", ["geodata"])
+
         return
 
-    def process(self, data):
-        self.log.info(data)
-        # TODO
-        return
+    def export(self, data):
+
+        geodata = data["geodata"]
+
+        base.utils.validate_geodata(geodata)
+
+    # geodata = run['item']
+    # tool.info("Creating TIP file for {0}".format(geodata))
+    #
+    # row = run["item_row"]
+    # row_fields = run["item_row_fields"]
+    # tool.info(row_fields)
+    # tip_order = row[row_fields.index("tip_order")]
+    # tip_order = tip_order.split(",")
+    # tool.info(tip_order)
+    # ordered_fields = [f for f in tip_order if f in tip_fields]
+    # tip_dic = OrderedDict()
+    # for tf in ordered_fields:
+    #     tip_dic[tf] = row[row_fields.index(tf)]
+    # # tip_dic = {tf: row[row_fields.index(tf)] for tf in ordered_fields}
+    #
+    # fpath, fname, fbase, fext = tool.split_up(geodata)
+    #
+    # tip_file = tool.join_up(tip_folder, fbase, ".tip")
+    # tool.info(tip_file)
+    # with open(tip_file, "w") as tipfile:
+    #     for k, v in tip_dic.iteritems():
+    #         tipfile.write("{0}: {1}\n".format(k, v))
+    #
+    # return {"item": geodata, "metadata": "to do", "tip_file": tip_file}
 
--- tools/metadata/export_xml.py
+++ tools/metadata/export_xml.py
@@ -1,32 +1,86 @@
 from base.base_tool import BaseTool
-from base.class_decorators import geodata, results
-from base.method_decorators import input_tableview, input_output_table
+from base import results
+from base import utils
+from base.method_decorators import input_tableview, input_output_table, parameter
+import arcpy
+import os
 
 tool_settings = {"label": "Export XML",
                  "description": "Exports XML...",
                  "can_run_background": "True",
-                 "category": "Metadata TODO"}
+                 "category": "Metadata"}
 
 
-@geodata
-@results
+def get_install_dir():
+
+    return arcpy.GetInstallInfo("desktop")["InstallDir"]
+
+
+def get_default_translator():
+
+    return os.path.join(get_install_dir(), "Metadata/Translator/ARCGIS2ISO19139.xml")  # ESRI_ISO2ISO19139.xml")
+
+
+def get_default_stylesheet():
+
+    return os.path.join(get_install_dir(), "Metadata/Stylesheets/ArcGIS.xsl")  # ESRI_ISO2ISO19139.xml")
+
+
+@results.result
 class ExportXmlMetadataTool(BaseTool):
+
     def __init__(self):
+
         BaseTool.__init__(self, tool_settings)
         self.execution_list = [self.iterate]
 
     @input_tableview("geodata_table", "Table of Geodata", False, ["geodata:geodata:"])
+    @parameter("xml_folder", "XML Folder", "DEFolder", "Required", False, "Input", None, None, None, None)
+    @parameter("translator", "Translator", "DEFile", "Required", False, "Input", None, None, None, get_default_translator(), None)
+    @parameter("stylesheet", "Style Sheet", "DEFile", "Required", False, "Input", None, None, None, get_default_stylesheet(), None)
     @input_output_table
     def getParameterInfo(self):
+
         return BaseTool.getParameterInfo(self)
 
     def iterate(self):
-        self.iterate_function_on_parameter(self.process, "geodata_table", ["geodata"])
+
+        self.iterate_function_on_parameter(self.export, "geodata_table", ["geodata"])
+
         return
 
-    def process(self, data):
-        self.log.info(data)
-        # TODO
-        return
+    def export(self, data):
 
+        geodata = data["geodata"]
+        self.log.info("Creating XML/HTML files for {}".format(geodata))
+
+        fpath, fname, fbase, fext = utils.split_up_filename.split_up(geodata)
+
+        xml_file = utils.join_up_filename(fpath, fbase, ".xml")
+        arcpy.ExportMetadata_conversion(geodata, self.translator, xml_file)
+        self.log.info("XML file '{}' created".format(xml_file))
+
+        html_file = utils.join_up_filename(self.xml_folder, fbase, ".html")
+        arcpy.XSLTransform_conversion(geodata, self.stylesheet, html_file, "#")
+        self.log.info("HTML file '{}' created".format(html_file))
+
+        return {"item": geodata, "xml_file": xml_file, "html_file": html_file}
+
+
+# import arcpy
+# from arcpy import env
+# env.workspace = "C:/data"
+# # set local variables
+# dir = arcpy.GetInstallInfo("desktop")["InstallDir"]
+# translator = dir + "Metadata/Translator/ESRI_ISO2ISO19139.xml"
+# arcpy.ExportMetadata_conversion("data.gdb/roads", translator,
+#                                 "roads_19139.xml")
+
+# import arcpy
+# from arcpy import env
+# env.workspace = "C:/data"
+# #set local variables
+# dir = arcpy.GetInstallInfo("desktop")["InstallDir"]
+# xslt = dir + "Metadata/Stylesheets/ArcGIS.xsl"
+# arcpy.XSLTransform_conversion("vegetation", xslt, "vegetation.html", "#")
 
